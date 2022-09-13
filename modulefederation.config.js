47b99c633b4d444285ee249e289e24ec37f8f08b
Emiliano <emiquiroga@andreani.com>
2022-09-13 14:04:27 -0300

    added dep for tests

--- modulefederation.config.js
+++ modulefederation.config.js
@@ -33,5 +33,17 @@ module.exports = {
       singleton: true,
       requiredVersion: dependencies["react-router-dom"],
     },
+    "@azure/msal": {
+      singleton: true,
+      requiredVersion: dependencies["@azure/msal"],
+    },
+    "@azure/msal-browser": {
+      singleton: true,
+      requiredVersion: dependencies["@azure/msal-browser"]
+    },
+    "@azure/msal-react": {
+      singleton: true,
+      requiredVersion: dependencies["@azure/msal-react"]
+    },
   },
 };
